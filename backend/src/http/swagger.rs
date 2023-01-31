5158963dd959edee7919508b4ef0e6e84e658c65
Maksych <maxim.18.08.1997@gmail.com>
2023-01-31 20:25:23 +0200

    swagger, readme

--- backend/src/http/swagger.rs
+++ backend/src/http/swagger.rs
@@ -1,13 +1,14 @@
 use axum::{response::Html, routing::get, Json, Router};
 
-pub async fn create_router() -> Result<Router, anyhow::Error> {
-    let swagger_html = include_str!("../../swagger/index.html");
+static SWAGGER_HTML: &str = include_str!("../../swagger/index.html");
 
-    let openapi_json =
-        serde_yaml::from_str::<serde_json::Value>(include_str!("../../swagger/openapi.yaml"))?;
+static OPENAPI_YAML: &str = include_str!("../../swagger/openapi.yaml");
+
+pub async fn create_router() -> Result<Router, anyhow::Error> {
+    let openapi_json = serde_yaml::from_str::<serde_json::Value>(OPENAPI_YAML)?;
 
     let router = Router::new()
-        .route("/", get(move || async move { Html(swagger_html) }))
+        .route("/", get(|| async { Html(SWAGGER_HTML) }))
         .route(
             "/openapi.json",
             get(move || async move { Json(openapi_json) }),
